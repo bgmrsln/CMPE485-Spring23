36c448afd966639b55c61d47dbaa09da901c115d
Begüm Arslan <begumarslan@outlook.de>
2023-03-12 00:37:26 +0300

    2.12 (Optional) Making the camera follow the player. I initially show a broader view of the game showing all cubes When they hit the ground, with a script ı make camera to follow cube2 - which is player- by making camera its child.

---
+++ HW#1/Assets/Scripts/CameraStarter.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStarter : MonoBehaviour
+{
+	[SerializeField] private GameObject objectToFollow;
+    [SerializeField] private float xOffset = -40f;
+    [SerializeField] private float yOffset = 5f;
+    [SerializeField] private float zOffset = -40f;
+    private bool isFollowingObject = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+    	if (objectToFollow != null && !isFollowingObject)
+        {
+            // Check if the object has hit the floor
+            if (objectToFollow.transform.position.y <= 0.5f)
+            {
+                // Set the camera position to follow the object
+                Vector3 position = objectToFollow.transform.position + new Vector3(xOffset, yOffset, zOffset);
+                Camera.main.transform.position = position;
+                //Camera.main.transform.rotation = Quaternion.identity;
+                Camera.main.transform.SetParent(objectToFollow.transform);
+                Camera.main.transform.LookAt(objectToFollow.transform.position);
+
+                
+                isFollowingObject = true;
+            }
+        }
+        
+    }
+}
